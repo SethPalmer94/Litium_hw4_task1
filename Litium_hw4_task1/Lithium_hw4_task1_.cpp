880442a542317dc38635ac229ba67e83344154db
jaxongreenwood <jaxongreenwood@weber.mail.edu>
2017-10-12 13:33:16 -0600

    randl

--- Litium_hw4_task1/Lithium_hw4_task1_.cpp
+++ Litium_hw4_task1/Lithium_hw4_task1_.cpp
@@ -1,16 +1,16 @@
 #include <iostream>
 #include <string>
-#include <conio.h>
+//#include <conio.h>
 using namespace std;
 /*Task:You are being requested to add new features to your House Compare Prices software (homework #2).
 The new requirement is to compare the price of three houses. For this you a required to do the following:
-•Update your code to use OOP. That is update your structure to a class (HouseInfo).
-•Your class should hold the following information
+?Update your code to use OOP. That is update your structure to a class (HouseInfo).
+?Your class should hold the following information
 oThe street number using an integer
 oThe house price using a double
 oThe street name using a string instead of an array of characters.
-oA method to update the object’s information based on the user input.
-•A function (ComparePrices) that takes an array of objects of type class HouseInfo that displays the cheapest of the three houses.
+oA method to update the object?s information based on the user input.
+?A function (ComparePrices) that takes an array of objects of type class HouseInfo that displays the cheapest of the three houses.
 If two houses have the same value, you should display both houses that are equally cheap.  */
 class HouseInfo
 {
@@ -37,12 +37,12 @@ public:
 	}
 	void ComparePrices(HouseInfo h1)
 	{
-
+		//random comment
 	}
 };
 int main()
 {
-	cout << "press any key to end program" << endl;
-	_getch();
+
+//	_getch();
 	return 0;
 }
